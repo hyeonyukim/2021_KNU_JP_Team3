4e457e45167ee16ca57475e169447a47b279b9c4
hyeonyukim <pinklady235@naver.com>
2021-03-15 14:08:21 +0900

    Add files via upload

---
+++ chess.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int m, n;
+    char ** map;
+    int i;
+    scanf("%d %d", &m, &n);
+    map = (char **)malloc(sizeof(char*)*m);
+    for(i=0; i<m; i++){
+        map[i] = char(*)
+    }
+}
